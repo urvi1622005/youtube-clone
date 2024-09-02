61d564fb145fc666b6102edbb2bf3d0b88cf6a80
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-09-03 00:16:02 +0530

    home..

--- youtube-clone/src/Components/Home.jsx
+++ youtube-clone/src/Components/Home.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Flex } from '@chakra-ui/react';
 import Sidebar from './Sidebar';
+import Navbar from './Navbar';
 import Videos from './Videos';
 
 const Home = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setSidebarOpen(!isSidebarOpen); // Toggle sidebar
+  };
+
   return (
     <Flex>
-      <Sidebar />
-      <Box flex="1" p="4">
-        <Videos />
+      <Sidebar isOpen={isSidebarOpen} />
+      <Box flex="1" ml={isSidebarOpen ? "64" : "0"} transition="margin-left 0.3s ease" bg="gray-900" minH="100vh">
+        <Navbar onMenuClick={handleMenuClick} />
+        <Box p="4">
+          <Videos />
+        </Box>
       </Box>
     </Flex>
   );
