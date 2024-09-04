4fbe18e1f9e6af6b1d80cafa8f771ec498006ae1
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-09-04 08:31:48 +0530

    app

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import VideoPlayer from './components/VideoPlayer';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Home from './Components/Home';
+import VideoPlayer from './Components/VideoPlayer';
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/video/:id" element={<VideoPlayer />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/video/:id" element={<VideoPlayer />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
