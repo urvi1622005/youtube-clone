07dc22ecacdbb08b681db75ab369d995f3504dae
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-09-11 07:18:26 +0530

    main

--- youtube-clone/src/main.jsx
+++ youtube-clone/src/main.jsx
@@ -1,10 +1,12 @@
-// src/main.jsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 // import theme from './theme';
 import './index.css';
+import './services/fetchvideos.js';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
