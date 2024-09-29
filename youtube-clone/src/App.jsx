5e245444bf6401ccbaf541bd87f1674bfd4dcf4f
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-09-29 19:25:30 +0530

    .

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './Components/Home';
-import VideoDetail from './Components/VideoDetail.jsx';
+import VideoDetail from './Components/VideoDetail'; // Correct import with default export
 
 // Create a client
 const queryClient = new QueryClient();
